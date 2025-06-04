41a5c5b0df6d6e4ade24c3c4ca9a8ddd280c17a6
Fitri Ariyanto <seorang.fitri@gmail.com>
2025-06-04 11:07:52 +0700

    Add default local time

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:intl/intl.dart';
 import 'package:just_audio_background/just_audio_background.dart';
 import 'package:al_ukhuwah/app.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -15,6 +16,7 @@ import 'firebase_options.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await _initFirebase();
+  Intl.defaultLocale = 'id_ID';
 
   final currentTheme = await AdaptiveTheme.getThemeMode();
   runApp(
